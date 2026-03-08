73e1f7b2995316fddc6805722c5203ad6d44eb39
Teresa Martínez Díaz <teresamartinezz094@gmail.com>
2026-03-08 20:42:32 +0100

    he puesto un mensaje

---
+++ script prueba.R
@@ -0,0 +1,8 @@
+#prueba
+#hola
+library(clickR)
+#git config --global user.name "Teresa Martínez Díaz"
+#git config --global user.email "teresamartinezz094@gmail.com"
+
+system("git config --gobal user.name 'Teresa Martínez Díaz'")
+system("git config --global user.email 'teresamartinezz094@gmail.com'")
