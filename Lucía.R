f40c5f9500cbd33bc45d1cbdb8d290f253e46a0d
Lucía <luciafustermareny@gmail.com>
2026-03-12 12:26:35 +0100

    CAMBIOS

--- Lucía.R
+++ Lucía.R
@@ -3,8 +3,26 @@
 #probando
 #Lo hemos conseguidooooo
 
-#probando otra veez
 
-#otraa
-#holaaa
+library(clickR)
+datos <- read.csv("estudiantes.csv", header=TRUE, sep=";")
+dim(datos)
+head(datos)
 
+table(datos$Father.s.occupation)
+table(datos$Mother.s.occupation)
+
+#Vemos que hay 19 faltantes en ocupación de padre y 17 en ocupacion de la madre
+#siendo aproximadamente un 0,9% de todos los datos
+
+#vemos cuantos valores desconocidos hay
+table(datos$Mother.s.qualification)
+#hay 130 valores desconocidos
+
+table(datos$Father.s.qualification)
+#hay 112 valores desconocidos
+
+#hay un total de 6,28% de faltantes si contamos los desconocidos como faltantes
+
+
+mine.plot(datos)
