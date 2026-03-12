9db5f46f9f43f2346e65b8857263606ef416b3eb
Yalda377 <yaldaalinajaddan@gmail.com>
2026-03-12 11:25:15 +0100

    He añadido el análisis de la variable Target

--- Yalda.R
+++ Yalda.R
@@ -1,2 +1,12 @@
 #Espacio de Yalda
-hola45
+#Análisis inicial de la variable Target
+
+#Frecuencias absolutas
+freq_target <- table(datos$Target)
+freq_target
+#Frecuencias relativas
+prop.table(freq_target)
+
+#Porcentajes
+porcentajes = prop.table(freq_target) * 100
+porcentajes
