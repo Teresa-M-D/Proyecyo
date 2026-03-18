188122b9a5247a7fcbca7794dc106b488a7e6613
Lucía <luciafustermareny@gmail.com>
2026-03-18 13:42:07 +0100

    c

--- Lucía.R
+++ Lucía.R
@@ -26,3 +26,29 @@ table(datos$Father.s.qualification)
 
 
 mine.plot(datos)
+
+
+#Convertir "unknown" en NA:
+unique(datos$Mother.s.qualification)
+datos$Mother.s.qualification[datos$Mother.s.qualification == 34] <- NA
+datos$Father.s.qualification[datos$Father.s.qualification == 34] <- NA
+datos$Mother.s.occupation[datos$Mother.s.occupation == 99] <- NA
+datos$Father.s.occupation[datos$Father.s.occupation == 99] <- NA
+
+
+descriptive(datos)
+
+
+#Seleccionar moda de Mother.s.occupation y Father.s.occupation:
+
+
+descriptive(datos$Mother.s.occupation)
+table(datos$Mother.s.occupation)
+datos$Mother.s.occupation[is.na(datos$Mother.s.occupation)] <- "9"
+descriptive(datos$Father.s.occupation)
+datos$Father.s.occupation[is.na(datos$Father.s.occupation)] <- "9"
+
+
+colSums(is.na(datos))
+sum(is.na(datos))
+
