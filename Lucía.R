3c342fd8b80967ee00b8901de0ba559ecb7ee795
Lucía <luciafustermareny@gmail.com>
2026-03-25 12:54:31 +0100

    sffg

--- Lucía.R
+++ Lucía.R
@@ -1,13 +1,51 @@
 #espacio para Lucía
 
-#probando
-#Lo hemos conseguidooooo
+#BASE DE DATOS CON DATOS IMPUTADOS POR MODA:
+library(clickR)
 
+datos_imputados <- read.csv("estudiantes.csv", header = TRUE, sep = ";")
+
+# Convertimos los códigos de desconocido a NA
+datos_imputados$Mother.s.qualification[datos_imputados$Mother.s.qualification == 34] <- NA
+datos_imputados$Father.s.qualification[datos_imputados$Father.s.qualification == 34] <- NA
+datos_imputados$Mother.s.occupation[datos_imputados$Mother.s.occupation == 99] <- NA
+datos_imputados$Father.s.occupation[datos_imputados$Father.s.occupation == 99] <- NA
+
+# Comprobación
+sum(is.na(datos_imputados$Mother.s.occupation))
+sum(is.na(datos_imputados$Father.s.occupation))
+sum(is.na(datos_imputados$Mother.s.qualification))
+sum(is.na(datos_imputados$Father.s.qualification))
+sum(is.na(datos_imputados))
+
+descriptive(datos_imputados)
+
+# Imputación por moda (sin comillas)
+datos_imputados$Mother.s.occupation[is.na(datos_imputados$Mother.s.occupation)] <- "9"
+datos_imputados$Father.s.occupation[is.na(datos_imputados$Father.s.occupation)] <- "9"
+datos_imputados$Mother.s.qualification[is.na(datos_imputados$Mother.s.qualification)] <- "1"
+datos_imputados$Father.s.qualification[is.na(datos_imputados$Father.s.qualification)] <- "37"
+
+# Comprobación
+sum(is.na(datos_imputados$Mother.s.occupation))
+sum(is.na(datos_imputados$Father.s.occupation))
+sum(is.na(datos_imputados$Mother.s.qualification))
+sum(is.na(datos_imputados$Father.s.qualification))
+sum(is.na(datos_imputados))
+
+descriptive(datos_imputados)
+
+
+
+#hasta aquí
 
 library(clickR)
 datos <- read.csv("estudiantes.csv", header=TRUE, sep=";")
+datos_imputados <- read.csv("estudiantes.csv", header=TRUE, sep=";")
 dim(datos)
 head(datos)
+
+
 
 table(datos$Father.s.occupation)
 table(datos$Mother.s.occupation)
@@ -29,14 +67,14 @@ mine.plot(datos)
 
 
 #Convertir "unknown" en NA:
-unique(datos$Mother.s.qualification)
-datos$Mother.s.qualification[datos$Mother.s.qualification == 34] <- NA
-datos$Father.s.qualification[datos$Father.s.qualification == 34] <- NA
-datos$Mother.s.occupation[datos$Mother.s.occupation == 99] <- NA
-datos$Father.s.occupation[datos$Father.s.occupation == 99] <- NA
+unique(datos_imputados$Mother.s.qualification)
+datos_imputados$Mother.s.qualification[datos_imputados$Mother.s.qualification == 34] <- NA
+datos_imputados$Father.s.qualification[datos_imputados$Father.s.qualification == 34] <- NA
+datos_imputados$Mother.s.occupation[datos_imputados$Mother.s.occupation == 99] <- NA
+datos_imputados$Father.s.occupation[datos_imputados$Father.s.occupation == 99] <- NA
 
 
-descriptive(datos)
+descriptive(datos_imputados)
 
 
 #Seleccionar moda de Mother.s.occupation y Father.s.occupation:
@@ -46,22 +84,24 @@ descriptive(datos$Mother.s.occupation)
 table(datos$Mother.s.occupation) 
 
 #para ver cuantos faltantes tenemos
-colSums(is.na(datos))
+colSums(is.na(datos_imputados))
 
 #imputamos los NA de esta variable con su moda y así con las otras tres variables
 #(Father.s.occupation, Mother.s.qualification, Father.s.qualification)
-datos$Mother.s.occupation[is.na(datos$Mother.s.occupation)] <- "9"
-descriptive(datos$Father.s.occupation)
-datos$Father.s.occupation[is.na(datos$Father.s.occupation)] <- "9"
+datos_imputados$Mother.s.occupation[is.na(datos_imputados$Mother.s.occupation)] <- "9"
+descriptive(datos_imputados$Father.s.occupation)
+datos_imputados$Father.s.occupation[is.na(datos_imputados$Father.s.occupation)] <- "9"
 
-table(datos$Mother.s.qualification)
-datos$Mother.s.qualification[is.na(datos$Mother.s.qualification)] <- "1"
+table(datos_imputados$Mother.s.qualification)
+datos_imputados$Mother.s.qualification[is.na(datos_imputados$Mother.s.qualification)] <- "1"
 
-table(datos$Father.s.qualification)
-datos$Father.s.qualification[is.na(datos$Father.s.qualification)] <- "37"
+table(datos_imputados$Father.s.qualification)
+datos_imputados$Father.s.qualification[is.na(datos_imputados$Father.s.qualification)] <- "37"
 
 #vemos que en efecto ya no quedan faltantes
-colSums(is.na(datos))
-sum(is.na(datos))
+colSums(is.na(datos_imputados))
+sum(is.na(datos_imputados))
 
 
+
+descriptive(datos_imputados)
