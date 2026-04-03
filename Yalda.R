3162bd3e35f2968290ce0e7cab448cf419934b61
Yalda377 <yaldaalinajaddan@gmail.com>
2026-04-03 19:46:54 +0200

    He hecho boxplots de la variables númericas y una transformación lineal a la variable Calificación de admisión

--- Yalda.R
+++ Yalda.R
@@ -10,3 +10,17 @@ prop.table(freq_target)
 #Porcentajes
 porcentajes = prop.table(freq_target) * 100
 porcentajes 
+
+#Valores atípicos en variables numéricas
+boxplot(datos$Previous.qualification..grade., main = "Calificación de admisión (del 0 a 200)")
+boxplot(datos$Unemployment.rate, main= "Tasa de desmpleo")
+boxplot(datos$Inflation.rate, main= "Tasa de inflación")
+boxplot(datos$GDP, main= "PIB")
+
+#No tiene sentido hacer boxplot de variables numericas discretas!!!!
+
+
+#Transformación lineal de la variable Calificación de admisión, en concreto, un cambio de escala (del 1 al 10)
+datos$NotaAdmisión <- (datos$Previous.qualification..grade. / 200) * 10
+descriptive(datos)
+boxplot(datos$NotaAdmisión, main="Nota de Admisión (del 0 al 10)")
