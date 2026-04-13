ae60207fa326e8ba46cb2d3d33f875d875ee559d
Teresa M <teresamartinezz094@gmail.com>
2026-04-13 16:07:50 +0200

    voy adelantando

--- Teresa.R
+++ Teresa.R
@@ -42,57 +42,25 @@ variables_numéricas<-c("Previous.qualification.grade_10",
 describe(datos_recodificados[,variables_numéricas])
 
 
-#EVALUCIACIONES
-#1SEM
-#descriptive general
-descriptive(datos_recodificados$Curricular.units.2nd.sem..evaluations.)
-#descriptivo por pib/año
-tapply(datos_recodificados$Curricular.units.1st.sem..evaluations., datos_recodificados$PIB, descriptive) #sale lo http://127.0.0.1:44275/graphics/plot_zoom_png?width=1163&height=861del final pq descriptive es una funcion q cada vez q se eejcuta imprime aparte de return, entonces mientras se ejecuta me lo va enseñando, y al final me pone el vector del valor_PIB: resultado, para los tres casos: variable numerica(mi caso, evaluationes es numerica), categorica(no solo le hje dado una variable numerica asi q no sale nada) y fecha(lo mismo, null).
-#boxplot por pib
-boxplot(datos_recodificados$Curricular.units.1st.sem..evaluations.~datos_recodificados$PIB yaxt="n")
-axis(2, at=seq(0, 45, by=5))
-#curva densidad
-plot(density(datos_recodificados$Curricular.units.1st.sem..evaluations.))
-hist(datos_recodificados$Curricular.units.1st.sem..evaluations., breaks=seq(0, 46, by=1), xaxt="n")
-axis(1, at=seq(2, 46, by=1))
-describe(datos_recodificados$Curricular.units.1st.sem..evaluations.)
+#***********************
+#Aprobadas
+#***********************
+#tiene q ver las q apruebas con a cuantas te presentas?
+datos_recodificados$Curricular.units.1st.sem..approved.
+descriptive(datos_recodificados$Curricular.units.1st.sem..approved.)
+boxplot(datos_recodificados$Curricular.units.1st.sem..approved.~ datos_recodificados$Course, las=2)
+#otra vez salen los 0 raros de multimedia
+#vamos a ver si dpendn del año (aunq me paree raro q puedan aprobar)
+ggplot(datos_recodificados, 
+       aes(x=factor(PIB), y=Curricular.units.1st.sem..approved.)
+       )+
+geom_boxplot()+
+facet_wrap(~Course)+
+labs(x="PIB", y="Unidades curriculares aprobadas", title="Unidades curriculares aprobadas en cada año según la carrera")
 
-#calculo moda (no hay una fucnion)
-names(sort(table(datos_recodificados$Curricular.units.1st.sem..evaluations.), decreasing = TRUE))[1]
-x<-prop.table(table(datos_recodificados$Curricular.units.1st.sem..evaluations.))
-barplot(x)      
-#2SEM
-#descriptive general
-descriptive(datos_recodificados$Curricular.units.1st.sem..evaluations.)
-#descriptivo por pib/año
-tapply(datos_recodificados$Curricular.units.2nd.sem..evaluations., datos_recodificados$PIB, descriptive) #sale lo del final pq descriptive es una funcion q cada vez q se eejcuta imprime aparte de return, entonces mientras se ejecuta me lo va enseñando, y al final me pone el vector del valor_PIB: resultado, para los tres casos: variable numerica(mi caso, evaluationes es numerica), categorica(no solo le hje dado una variable numerica asi q no sale nada) y fecha(lo mismo, null).
-#boxplot por pib
-boxplot(datos_recodificados$Curricular.units.1st.sem..evaluations.~datos_recodificados$PIB yaxt="n")
-axis(2, at=seq(0, 45, by=5))
-#curva densidad
-plot(density(datos_recodificados$Curricular.units.1st.sem..evaluations.))
-hist(datos_recodificados$Curricular.units.1st.sem..evaluations., breaks=seq(0, 46, by=1), xaxt="n")
-axis(1, at=seq(2, 46, by=1))
-describe(datos_recodificados$Curricular.units.1st.sem..evaluations.)
 
-#calculo moda (no hay una fucnion)
-names(sort(table(datos_recodificados$Curricular.units.1st.sem..evaluations.), decreasing = TRUE))[1]
-x<-prop.table(table(datos_recodificados$Curricular.units.1st.sem..evaluations.))
-barplot(x)   
-
-#ahora separo según las carreras
-boxplot(datos_recodificados$Curricular.units.1st.sem..evaluations.~datos_recodificados$Course, las=2, cex.axis=0.6) #las=2 para poner verticales los nombres y así se ven, 
-par(mar=c(13,4,4,2))  #para dejar más espacio abajo (el primer elemento del vector es el espacio de abajo)
-#descriptivo  e histograma de evaluaciones en multimedia especificamente
-
-descriptive(datos_recodificados$Curricular.units.2nd.sem..evaluations.[datos_recodificados$Course=="Diseño de Animación y Multimedia"])
-unique(datos_recodificados$Course)
-
-hist(datos_recodificados$Curricular.units.1st.sem..evaluations.[datos_recodificados$Course=="Diseño de Animación y Multimedia"])
-#comparo carrera con dropout
-tabla<-table(datos_recodificados$Course, datos_recodificados$Target)
-mosaicplot(tabla, las=2, cex.axis = 0.7)#hay gente parecida en multimedia q en el resto, entonce no entiendo muy bien pq hay tan pocas evaluaciones
-
-#descriptivo por carrera:
-tapply(datos_recodificados$Curricular.units.1st.sem..evaluations., datos_recodificados$Course, descriptive)
+#Creo nueva variable
+datos_recodificados$Porcentaje_aprobado_sem_1<-100*(datos_recodificados$Curricular.units.1st.sem..approved./datos_recodificados$Curricular.units.1st.sem..evaluations.)
+descriptive(datos_recodificados$Porcentaje_aprobado_sem_1)
+boxplot(datos_recodificados$Porcentaje_aprobado_sem_1~ datos_recodificados$Course)
 
