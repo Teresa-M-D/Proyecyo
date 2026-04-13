4ed6aefbe98a5faffbbe3c63a727367d812ddd52
Teresa M <teresamartinezz094@gmail.com>
2026-04-13 22:03:35 +0200

    asefcdzfgb

--- univariante variables no curr. units.R
+++ univariante variables no curr. units.R
@@ -48,3 +48,57 @@ qqnorm(
 )
 qqline(datos_recodificados$Age.at.enrollment)
 grid()
+
+
+
+
+#*********************
+#Application order
+#*********************
+descriptive(datos_recodificados$Application.order)
+tabla_orden<-table(datos_recodificados$Application.order)
+barplot(tabla_orden,  yaxt="n")
+axis(2, at=seq(0, 3500, by=100), las=2)
+#para ver si la opcion 0 es porque la forma de aplicqacion es distita, voy a ver si todos los de 0 no vienen del metodo normal
+tabla_aplicacion<-table(datos_recodificados$Application.order, datos_recodificados$Application.mode)
+tabla_aplicacion
+unique(datos_recodificados$Application.mode)
+chisq.test(tabla_aplicacion, simulate.p.value = TRUE)
+sum(datos_recodificados$Application.order[datos_recodificados$Application.mode!="Titulares de otros estudios superiores"]==0)
+mosaicplot(tabla_aplicacion, las=2)
+#ppexp
+#como es discreta, no se puede ver si cuadra con exponenecial con ppexp, entonces vemos otro metodo
+plot(
+  as.numeric(names(table(datos_recodificados$Application.order))),
+  as.numeric((table(datos_recodificados$Application.order))),
+  pch = 19,
+  xlab = "Application order",
+  ylab = "Frecuencia"
+)
+# en general el dibujo se parece a la exponenecial, entonces:
+x <- as.numeric(names((table(datos_recodificados$Application.order))))
+y <- log(as.numeric((table(datos_recodificados$Application.order))))
+
+modelo_exp <- lm(y ~ x)
+summary(modelo_exp)
+#no es exponencial
+
+#***********
+#Inflation rate
+#***********
+descriptive(datos_recodificados$Inflation.rate)
+barplot(table(datos_recodificados$Inflation.rate))
+table(datos_recodificados$Inflation.rate)
+#***********
+#PIB
+#***********
+descriptive(datos_recodificados$PIB)
+barplot(table(datos_recodificados$PIB))
+table(datos_recodificados$PIB)
+#***********
+#Unemployment
+#***********
+descriptive(datos_recodificados$Unemployment.rate)
+barplot(table(datos_recodificados$Unemployment.rate))
+table(datos_recodificados$Unemployment.rate)
+
