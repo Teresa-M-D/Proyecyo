5ee058eececb8a6109eaa72cbf2e2b7021087954
Teresa M <teresamartinezz094@gmail.com>
2026-04-13 19:53:08 +0200

    más univariante

--- Teresa.R
+++ Teresa.R
@@ -42,38 +42,3 @@ variables_numéricas<-c("Previous.qualification.grade_10",
 describe(datos_recodificados[,variables_numéricas])
 
 
-#***********************
-#Aprobadas
-#***********************
-#1SEM
-#tiene q ver las q apruebas con a cuantas te presentas?
-datos_recodificados$Curricular.units.1st.sem..approved.
-descriptive(datos_recodificados$Curricular.units.1st.sem..approved.)
-boxplot(datos_recodificados$Curricular.units.1st.sem..approved.~ datos_recodificados$Course, las=2)
-#otra vez salen los 0 raros de multimedia
-#vamos a ver si dpendn del año (aunq me paree raro q puedan aprobar)
-ggplot(datos_recodificados, 
-       aes(x=factor(PIB), y=Curricular.units.1st.sem..approved.)
-       )+
-geom_boxplot()+
-facet_wrap(~Course)+
-labs(x="PIB", y="Unidades curriculares aprobadas", title="Unidades curriculares aprobadas en cada año según la carrera")
-
-
-#Creo nueva variable
-datos_recodificados$Porcentaje_aprobado_sem_1<-100*(datos_recodificados$Curricular.units.1st.sem..approved./datos_recodificados$Curricular.units.1st.sem..evaluations.)
-descriptive(datos_recodificados$Porcentaje_aprobado_sem_1)
-boxplot(datos_recodificados$Porcentaje_aprobado_sem_1~ datos_recodificados$Course, las=2, cex.axis=0.6) #hay q bajar tamaño letra
-par(mar=c(13,4,4,2))
-#aproxmamos a normal
-qqnorm(
-  datos_recodificados$Curricular.units.1st.sem..approved.,
-  main = "Papel probabilístico normal Aprobados 1º sem.",
-)
-qqline(datos_recodificados$Curricular.units.1st.sem..approved.)
-grid()
-
-#2SEM
-descriptive(datos_recodificados$Curricular.units.2nd.sem..approved.)
-boxplot(datos_recodificados$Curricular.units.1st.sem..approved.~ datos_recodificados$Course, las=2, cex.axis=0.6)
-
