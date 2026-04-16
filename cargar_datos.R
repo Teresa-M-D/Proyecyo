03b99a1019a16693606267daff6eb42dcfc1a625
Lucía <luciafustermareny@gmail.com>
2026-04-16 11:01:20 +0200

    He añadido una nueva variable

--- cargar_datos.R
+++ cargar_datos.R
@@ -730,3 +730,7 @@ datos_recodificados$Father_occupation_level <- case_when(
 #NUEVA VARIABLE: (Lo añado ahora)
 datos_recodificados$Porcentaje_aprobado_sem_1<-100*(datos_recodificados$Curricular.units.1st.sem..approved./datos_recodificados$Curricular.units.1st.sem..evaluations.)
 
+#NUEVA VARIABLE:
+datos_recodificados$Carga_academica_real <- 
+  datos_recodificados$Curricular.units.1st.sem..enrolled. - 
+  datos_recodificados$Curricular.units.1st.sem..credited.
